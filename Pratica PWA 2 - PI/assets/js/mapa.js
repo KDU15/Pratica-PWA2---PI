340ce3e1355b9b33204a4c440979b0c2fd8d7828
KDU15 <92228051+KDU15@users.noreply.github.com>
2022-10-20 11:23:12 -0300

    Add files via upload

---
+++ Pratica PWA 2 - PI/assets/js/mapa.js
@@ -0,0 +1,36 @@
+var map = L.map('map').setView([-8.1158, -34.8986], 12);
+
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+subdomains: 'abcd',
+	maxZoom: 19
+});
+CartoDB_DarkMatter.addTo(map);
+
+// Google Map Layer
+
+googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+ });
+ googleStreets.addTo(map);
+
+ // Satelite Layer
+googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+   maxZoom: 20,
+   subdomains:['mt0','mt1','mt2','mt3']
+ });
+googleSat.addTo(map);
+
+var baseLayers = {
+    "Satellite":googleSat,
+    "Google Map":googleStreets,
+    "OpenStreetMap": osm,
+};
+
+L.Control.geocoder().addTo(map);
--- Pratica PWA 2 - PI/assets/js/prop.js
+++ Pratica PWA 2 - PI/assets/js/prop.js
@@ -1,3 +1,12 @@
 function NavegacaoPorPaginas(a){
     window.location.href = a;
 };
+
+function AbrirPesquisa(){
+    if($(".leaflet-control-geocoder"). hasClass("leaflet-control-geocoder-expanded")){
+        $(".leaflet-control-geocoder").removeClass("leaflet-control-geocoder-expanded");
+    }
+    else{
+        $(".leaflet-control-geocoder").addClass("leaflet-control-geocoder-expanded");
+    }
+};
